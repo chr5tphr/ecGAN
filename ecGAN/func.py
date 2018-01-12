17de381a6bcb70de7d343f42ac4daafceead46f5
Christopher <chrstphr@posteo.eu>
2018-01-12 18:21:57 +0100

    some initial z+ action

--- ecGAN/func.py
+++ ecGAN/func.py
@@ -1,6 +1,6 @@
 import mxnet as mx
 
-from mxnet import nd, gluon
+from mxnet import nd, gluon, autograd
 from mxnet.gluon import nn
 
 
@@ -10,8 +10,43 @@ def fuzzy_one_hot(x,size):
                     nd.uniform(low=0.0,high=0.3,shape=(x.shape[0],size),ctx=x.context))
 
 
-class Interpretable(nn.Block):
+class Interpretable(object):
+    def relevance(self,*args,method='dtd',**kwargs):
+        func = getattr(self,'relevance_'+method)
+        return func(*args,**kwargs)
+
+    def relevance_sensitivity(self):
+        # result = []
+        # for param in self.collect_params().items():
+        #     result.append()
+        # return self.collect_params()
+        raise NotImplementedError
+
+    def relevance_dtd(self,a,R):
+        raise NotImplementedError
+
+class Dense(nn.Dense,Interpretable):
+    def relevance_dtd(self,a,R):
+        wplus = nd.maximum(0.,self.weight.data())
+        with autograd.record():
+            z = nd.FullyConnected(a,wplus,None,no_bias=True)
+            # if self.act is not None:
+            #     z = self.act(z)
+        c = autograd.grad(z,a,head_grads=R/z)
+        return a*c
+
+class LeakyReLU(nn.LeakyReLU,Interpretable):
     pass
+
+class Sequential(nn.Sequential,Interpretable):
+    def relevance(self,x,y=None,method='dtd'):
+        A = [x]
+        for child in self._children:
+            A.append(child.forward(Z[-1]))
+        R = [A.pop() if y is None else y]
+        for child,a in zip(self._children[::-1],Z[::-1]):
+            S.append(child.relevance(a,R[-1],method=method))
+        return R[-1]
 
 class MaxOut(nn.Block):
     pass
