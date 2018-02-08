f82cadb5a63f6f4bd25537577cdda2f033eb2781
Christopher <chrstphr@posteo.eu>
2018-02-08 19:17:23 +0100

    mxext working mxnet interface ndarray

--- src/mxext.cpp
+++ src/mxext.cpp
@@ -1,9 +1,7 @@
 #include "Python.h"
-// #include "mxnet-cpp/MxNetCpp.h"
+#include "mxnet/ndarray.h"
 
-// using namespace mxnet::cpp;
-
-
+//g++ -fPIC -shared -I/usr/include/python3.6m -I/home/chrstphr/libs/mxnet/{{.,nnvm,dmlc-core,dlpack}/include,mshadow} -o mxext.so mxext.cpp -DMSHADOW_USE_{MKL,CUDA}=0
 
 static PyObject * mxext_ndlist2raw(PyObject *self, PyObject *args) {
 
@@ -21,9 +19,25 @@ static PyObject * mxext_ndlist2raw(PyObject *self, PyObject *args) {
   PyObject *pyhandle = PyObject_GetAttrString(pyarr,"handle");
   PyObject *pyvalue = PyObject_GetAttrString(pyhandle,"value");
 
-  void *handle = PyLong_AsVoidPtr(pyvalue);
+  mxnet::NDArray *ndarr = static_cast<mxnet::NDArray*>(PyLong_AsVoidPtr(pyvalue));
 
-  return PyLong_FromLong((long)handle);
+  // std::vector<NDArray> data(num_args);
+  // std::vector<std::string> names;
+  // for (mx_uint i = 0; i < num_args; ++i) {
+  //   data[i] = *static_cast<NDArray*>(args[i]);
+  // }
+  // if (keys != nullptr) {
+  //   names.resize(num_args);
+  //   for (mx_uint i = 0; i < num_args; ++i) {
+  //     names[i] = keys[i];
+  //   }
+  // }
+  // {
+  //   dmlc::MemoryStringStream strm(&ret->ret_str);
+  //   mxnet::NDArray::Save(strm, data, names);
+  // }
+
+  return PyLong_FromLong((long)(ndarr->dtype()));
   // Py_RETURN_NONE;
   // return ;
 }
